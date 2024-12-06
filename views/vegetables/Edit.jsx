513cefbf44f22ac75f5a0b1f6656be7006b030c3
Carl Joseph <carljoseph156@gmail.com>
2024-12-05 21:27:24 -0500

    added vegetables view

---
+++ views/vegetables/Edit.jsx
@@ -0,0 +1,35 @@
+class Edit extends React.Component {
+  render() {
+    return (
+      <form
+        action={`/api/vegetables/${this.props.id}?_method=PUT`}
+        method="POST"
+      >
+        Name:{" "}
+        <input
+          type="text"
+          name="name"
+          defaultValue={this.props.vegetable.name}
+        />{" "}
+        <br />
+        Color:{" "}
+        <input
+          type="text"
+          name="color"
+          defaultValue={this.props.vegetable.color}
+        />{" "}
+        <br />
+        Is Ready to Eat:
+        {this.props.vegetable.readyToEat ? (
+          <input type="checkbox" name="readyToEat" defaultChecked />
+        ) : (
+          <input type="checkbox" name="readyToEat" />
+        )}
+        <br />
+        <input type="submit" name="" value="Edit Vegetable" />
+      </form>
+    );
+  }
+}
+
+module.exports = Edit;
